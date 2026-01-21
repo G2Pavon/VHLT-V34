2dd1253c2b1dc4f316dfa8346c8a65e4e506874e
G2Pavon <gasparguidopavon@gmail.com>
2026-01-21 19:16:05 -0300

    Cleanup: hullfile variables initialization and definition

--- src/hlcsg/hullfile.cpp
+++ src/hlcsg/hullfile.cpp
@@ -39,22 +39,18 @@ void LoadHullfile(const char *filename)
 
     float x1, y1, z1;
     float x2, y2, z2;
-    char magic;
 
     FILE *file = fopen(filename, "r");
 
-    int count;
-    int i;
-
-    magic = (char)fgetc(file);
+    char magic = (char)fgetc(file);
     rewind(file);
 
     if (magic == '(')
     { // Test for old-style hull-file
 
-        for (i = 0; i < NUM_HULLS; i++)
+        for (int i = 0; i < NUM_HULLS; i++)
         {
-            count = fscanf(file, "( %f %f %f ) ( %f %f %f )\n", &x1, &y1, &z1, &x2, &y2, &z2);
+            int count = fscanf(file, "( %f %f %f ) ( %f %f %f )\n", &x1, &y1, &z1, &x2, &y2, &z2);
             if (count != 6)
             {
                 Error("Could not parse old hull definition file '%s' (%d, %d)\n", filename, i, count);
@@ -72,9 +68,9 @@ void LoadHullfile(const char *filename)
     else
     {
         // Skip hull 0 (visibile polygons)
-        for (i = 1; i < NUM_HULLS; i++)
+        for (int i = 1; i < NUM_HULLS; i++)
         {
-            count = fscanf(file, "%f %f %f\n", &x1, &y1, &z1);
+            int count = fscanf(file, "%f %f %f\n", &x1, &y1, &z1);
             if (count != 3)
             {
                 Error("Could not parse new hull definition file '%s' (%d, %d)\n", filename, i, count);
