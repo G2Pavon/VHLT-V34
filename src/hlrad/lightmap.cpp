6ed38d68da05122192d1096ef32e92212710b1b1
G2Pavon <gasparguidopavon@gmail.com>
2026-01-24 00:01:49 -0300

    Use std::rand

--- src/hlrad/lightmap.cpp
+++ src/hlrad/lightmap.cpp
@@ -4736,10 +4736,10 @@ void FinalLightFace(const int facenum)
             {
                 if (g_colour_jitter_hack[0] || g_colour_jitter_hack[1] || g_colour_jitter_hack[2])
                     for (int i = 0; i < 3; i++)
-                        lbi[i] += g_colour_jitter_hack[i] * ((float)rand() / RAND_MAX - 0.5);
+                        lbi[i] += g_colour_jitter_hack[i] * ((float)std::rand() / RAND_MAX - 0.5);
                 if (g_jitter_hack[0] || g_jitter_hack[1] || g_jitter_hack[2])
                 {
-                    temp_rand = (float)rand() / RAND_MAX - 0.5;
+                    temp_rand = (float)std::rand() / RAND_MAX - 0.5;
                     for (int i = 0; i < 3; i++)
                         lbi[i] += g_jitter_hack[i] * temp_rand;
                 }
