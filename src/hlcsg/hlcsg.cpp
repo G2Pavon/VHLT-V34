61cd9ae3a3cb8292226a04864201dddbe22637c9
G2Pavon <gasparguidopavon@gmail.com>
2026-01-31 17:07:07 -0300

    Delete: csg command -nolog

--- src/hlcsg/hlcsg.cpp
+++ src/hlcsg/hlcsg.cpp
@@ -1273,7 +1273,6 @@ static void Usage()
     Log("    -lightdata #     : Alter maximum lighting memory limit (in kb)\n");
     Log("    -chart           : display bsp statitics\n");
     Log("    -low | -high     : run program an altered priority level\n");
-    Log("    -nolog           : don't generate the compile logfiles\n");
     Log("    -threads #       : manually specify the number of threads to run\n");
     Log("    -estimate        : display estimated time during compile\n");
     Log("    -verbose         : compile with verbose messages\n");
@@ -1322,7 +1321,6 @@ static void Settings()
     }
 
     Log("verbose               [ %7s ] [ %7s ]\n", g_verbose ? "on" : "off", DEFAULT_VERBOSE ? "on" : "off");
-    Log("log                   [ %7s ] [ %7s ]\n", g_log ? "on" : "off", DEFAULT_LOG ? "on" : "off");
 
     Log("developer             [ %7d ] [ %7d ]\n", g_developer, DEFAULT_DEVELOPER);
     Log("chart                 [ %7s ] [ %7s ]\n", g_chart ? "on" : "off", DEFAULT_CHART ? "on" : "off");
@@ -1454,10 +1452,6 @@ int main(const int argc, char **argv)
                 else if (!strcasecmp(argv[i], "-high"))
                 {
                     g_threadpriority = eThreadPriorityHigh;
-                }
-                else if (!strcasecmp(argv[i], "-nolog"))
-                {
-                    g_log = false;
                 }
                 else if (!strcasecmp(argv[i], "-noskyclip"))
                 {
