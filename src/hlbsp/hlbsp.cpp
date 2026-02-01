1b4f0c288627d34d5e54fc19d44623f1683ceb09
G2Pavon <gasparguidopavon@gmail.com>
2026-02-01 09:03:03 -0300

    Delete: bsp command -nolog

--- src/hlbsp/hlbsp.cpp
+++ src/hlbsp/hlbsp.cpp
@@ -1296,7 +1296,6 @@ static void Usage()
     Log("    -lightdata #   : Alter maximum lighting memory limit (in kb)\n");
     Log("    -chart         : display bsp statitics\n");
     Log("    -low | -high   : run program an altered priority level\n");
-    Log("    -nolog         : don't generate the compile logfiles\n");
     Log("    -threads #     : manually specify the number of threads to run\n");
     Log("    -estimate      : display estimated time during compile\n");
 
@@ -1337,7 +1336,6 @@ static void Settings()
     }
 
     Log("verbose             [ %7s ] [ %7s ]\n", g_verbose ? "on" : "off", DEFAULT_VERBOSE ? "on" : "off");
-    Log("log                 [ %7s ] [ %7s ]\n", g_log ? "on" : "off", DEFAULT_LOG ? "on" : "off");
     Log("developer           [ %7d ] [ %7d ]\n", g_developer, DEFAULT_DEVELOPER);
     Log("chart               [ %7s ] [ %7s ]\n", g_chart ? "on" : "off", DEFAULT_CHART ? "on" : "off");
     Log("estimate            [ %7s ] [ %7s ]\n", g_estimate ? "on" : "off", DEFAULT_ESTIMATE ? "on" : "off");
@@ -1606,10 +1604,6 @@ int main(const int argc, char **argv)
                 else if (!strcasecmp(argv[i], "-high"))
                 {
                     g_threadpriority = eThreadPriorityHigh;
-                }
-                else if (!strcasecmp(argv[i], "-nolog"))
-                {
-                    g_log = false;
                 }
 
                 else if (!strcasecmp(argv[i], "-nohull2"))
